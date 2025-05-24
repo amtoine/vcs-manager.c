1a15887ffc66da847b2af153617ba750cb124983
amtoine <stevan.antoine@gmail.com>
2025-05-24 16:40:10 +0200

    add da_append_many and da_reserve
    Signed-off-by: amtoine <stevan.antoine@gmail.com>

--- dynamic_array.h
+++ dynamic_array.h
@@ -1,15 +1,29 @@
-#define da_append(da, item)                                                    \
+#define da_reserve(da, expected_capacity)                                      \
   do {                                                                         \
-    if ((da)->size >= (da)->capacity) {                                        \
+    if ((expected_capacity) > (da)->capacity) {                                \
       if ((da)->capacity == 0) {                                               \
         (da)->capacity = 256;                                                  \
-      } else {                                                                 \
+      }                                                                        \
+      while ((expected_capacity) > (da)->capacity) {                           \
         (da)->capacity *= 2;                                                   \
       }                                                                        \
       (da)->items =                                                            \
           realloc((da)->items, (da)->capacity * sizeof(*(da)->items));         \
     }                                                                          \
-    (da)->items[(da)->size++] = item;                                          \
+  } while (0)
+
+#define da_append(da, item)                                                    \
+  do {                                                                         \
+    da_reserve((da), (da)->size + 1);                                          \
+    (da)->items[(da)->size++] = (item);                                        \
+  } while (0)
+
+#define da_append_many(da, new_items, new_items_size)                          \
+  do {                                                                         \
+    da_reserve((da), (da)->size + (new_items_size));                           \
+    memcpy((da)->items + (da)->size, (new_items),                              \
+           (new_items_size) * sizeof(*(da)->items));                           \
+    (da)->size += (new_items_size);                                            \
   } while (0)
 
 #define da_foreach(Type, it, da)                                               \
